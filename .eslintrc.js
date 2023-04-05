e8d167facce8ca6e44217950c8593ded28b573ae
rafaelmelo2 <rafinhalelograma@hotmail.com>
2023-04-05 02:18:55 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'max-len': ['error', { code: 80 }],
+  },
+};
---
+++ src/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
